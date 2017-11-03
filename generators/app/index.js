c5389517797b49891b50138a50d72ba265e39f92
Christopher Haugen <chaugen24@knights.ucf.edu>
2017-11-03 00:16:03 -0400

    generator(app) Greet the user and create top level prompts

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,13 +1,55 @@
 var Generator = require('yeoman-generator');
 
 module.exports = class extends Generator {
-
-    method1() {
-        this.log('method 1 just ran');
+    //Greet the user
+    initializing() {
+        this.log(`
+     .-::/:-'        
+    -++/-://+.         
+    -++.  '://-         Hello!         
+    ':o.   ./+/-        You've just launched the GOATstack generator
+    :o   -/o+/'                  
+    '.' -/oo//          Select from the list below to get started!
+        '/ooo+-'       
+        .+ooo--+o+:-.    
+        .oooo:':/+ooo.   
+        :ooooo++oooo+'   
+        -oooooooooo+.    
+        ./oooooo+-' 
+        '`);
     }
+    //Ask the user for their input
+    prompting() {
+        return this.prompt([{
+          type    : 'list',
+          name    : 'home',
+          message : `Select from the options below`,
+          choices: [ 
+                {type: 'separator', line: '----------- Start here --------------------'},
+                `Generate a new app`, 
+                {type: 'separator', line: '----------- Resources --------------------'},
+                "View Documentation",
+                "View Demo app",
+                {type: 'separator', line: '----------- Get involved --------------------'},
+                "Report a Bug",
+                "Make a Pull Request",
+                {type: 'separator', line: '----------- Created By --------------------'},
+                "LyghtWorks",
+                "Christopher Haugen",
+                "Jason Thomas",
+                {type: 'separator', line: '----------- Custom optimizations, tailored for your use-case! ------------'},
+                "It\'s dangerous to go alone! Hire us"
+            ]
+        }, {
+          type    : 'confirm',
+          name    : 'cool',
+          message : 'Would you like to enable the Cool feature?',
+          store   : true
+        }]).then((answers) => {
+          this.log('home selection', answers.home);
+          this.log('cool feature', answers.cool);
+        });
+      }
 
-    method2() {
-        this.log('method 2 just ran');
-    }
 
 };
